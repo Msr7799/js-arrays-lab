0ede65f1d9757022b19ba612d92e653e70882e84
Msr7799 <alromaihi2224@gmail.com>
2024-10-01 03:32:20 +0300

    the HW

---
+++ shosh.js
@@ -0,0 +1,22 @@
+for (let i = 15; i > 0 ; i--) {
+    console.log(i);
+  }
+
+
+
+
+//   for (let number = 1; number < 10; number++) {
+//     console.log(number);
+//   }
+
+
+
+  let i= 1;
+
+while ( i <= 10) {
+  console.log(number);
+  number++;
+
+  
+}
+sqqe 
